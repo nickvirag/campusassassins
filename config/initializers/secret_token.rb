69350c21ae1fe702cf690b1d5ee04ab89303ca66
Nick Virag <nickvirag516@gmail.com>
2014-02-21 19:30:14 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ltuassassins::Application.config.secret_token = '4580cb4ef628f0647d7cbb1ed136a49a65a8cdc987bad5c87283accf2a3ddf16e2275cb3414f98e01368dbc9b991830190e9bf089a696ac6ea3515fc0c1a21a0'
