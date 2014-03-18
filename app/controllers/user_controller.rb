db311636656f670a60c6c5015435c2ef1cd0a2da
Nick Virag <nickvirag516@gmail.com>
2014-03-17 21:17:40 -0400

    updates

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,18 @@
 class UserController < ApplicationController
 	def tag
-		render :text => "tagged " + Player.find( params[ :id ] ).user.name
-		
+		tagged = Player.find( params[ :id ] )
+		tagger = Player.find( params[ :tagger ] )
+		season = tagger.season
+		tagger.succeededtargets = tagger.addSucceededTarget( params[ :id ] )
+		tagger.currenttargets = tagger.removeCurrentTarget( params[ :id ] )
+		tagger.points += season.tagpoints
+		if( tagged.points - season.tagged_points < 0 )
+			tagged.points = 0
+		else
+			tagged.points -= season.tagged_points
+		end
+		tagger.save
+		tagged.save
+		redirect_to :controller => :home, :action => :index
 	end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -9,4 +9,38 @@ class Player < ActiveRecord::Base
 		end
 		t
 	end
+	def addSucceededTarget id
+		if( succeededtargets != nil )
+			succeededtargets += ","
+		else
+			succeededtargets = ""
+		end
+		succeededtargets += id.to_s
+		succeededtargets
+	end
+	def addFailedTarget id
+		if( failedtargets != nil )
+			failedtargets += ","
+		else
+			failedtargets = ""
+		end
+		failedtargets += id.to_s
+		failedtargets
+	end
+	def removeCurrentTarget id
+		t = ''
+		res = currenttargets.split( ',' ).map( &:to_i )
+		res.each do |i|
+			puts ( id.to_i - i.to_i ).to_s
+			if( i.to_i != id.to_i )
+				puts 'success with' + i.to_s
+				if t != ''
+					t += ','
+				end
+				t += i.to_s
+				puts t
+			end
+		end
+		t
+	end
 end
